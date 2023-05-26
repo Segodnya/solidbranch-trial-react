bf0ac76741fc7b37045e83c2081060292ed5a4ca
Kirill Kurentsov <immortalsteel@gmail.com>
2023-05-26 19:47:02 +0300

    Базовая функциональность

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import ListPage from './pages/ListPage';
+import SummaryPage from './pages/SummaryPage';
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Summary Page</Link>
+            </li>
+            <li>
+              <Link to="/navigator">List Page</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/navigator" element={<ListPage />} />
+          <Route path="/" element={<SummaryPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/SummaryPage.tsx
@@ -0,0 +1,51 @@
+import { Link } from 'react-router-dom';
+import jsonData from '../data.json';
+
+const SummaryPage = () => {
+  const data = jsonData.data;
+  const totalObjects = data.length;
+  const incomeCount = data.filter((item) => item.type === 'income').length;
+  const outcomeCount = data.filter((item) => item.type === 'outcome').length;
+  const loanCount = data.filter((item) => item.type === 'loan').length;
+  const investmentCount = data.filter((item) => item.type === 'investment').length;
+
+  return (
+    <div className="summary-page">
+      <div className="card heading-card">
+        <div className="card-body">
+          <h5 className="card-title">Total Objects</h5>
+          <p className="card-text">{totalObjects}</p>
+        </div>
+      </div>
+      <div className="card income-card">
+        <div className="card-body">
+          <h5 className="card-title">Income</h5>
+          <p className="card-text">{incomeCount}</p>
+          <Link to="/navigator?tab=0" className="card-link">
+            See all
+          </Link>
+        </div>
+      </div>
+      <div className="card outcome-card">
+        <div className="card-body">
+          <h5 className="card-title">Outcome</h5>
+          <p className="card-text">{outcomeCount}</p>
+        </div>
+      </div>
+      <div className="card loan-card">
+        <div className="card-body">
+          <h5 className="card-title">Loan</h5>
+          <p className="card-text">{loanCount}</p>
+        </div>
+      </div>
+      <div className="card investment-card">
+        <div className="card-body">
+          <h5 className="card-title">Investment</h5>
+          <p className="card-text">{investmentCount}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SummaryPage;
